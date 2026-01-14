680c8fff7cf849bb3c48e58835bd268475f2ee7d
rajkishor <kothiyalrajkishor1@gmail.com>
2026-01-15 00:07:56 +0530

    created getbyid route

--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createNote } from "../controllers/noteController.js";
+import { createNote, getNoteById, showNotes } from "../controllers/noteController.js";
 
 const noteRouter = express.Router();
 
 noteRouter.post("/create", createNote);
+noteRouter.get("/show", showNotes);
+noteRouter.get("/get/:id", getNoteById);
 
 export default noteRouter
