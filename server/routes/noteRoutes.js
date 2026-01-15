46348d5a0220a4415de888f9a94595186d95eb77
rajkishor <kothiyalrajkishor1@gmail.com>
2026-01-15 17:27:42 +0530

    connect routes with controller

--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createNote, getNoteById, showNotes } from "../controllers/noteController.js";
+import { createNote, deleteNoteById, getNoteById, showNotes, updateNoteById } from "../controllers/noteController.js";
 
 const noteRouter = express.Router();
 
 noteRouter.post("/create", createNote);
 noteRouter.get("/show", showNotes);
 noteRouter.get("/get/:id", getNoteById);
+noteRouter.put("/update/:id", updateNoteById)
+noteRouter.delete("/delete/:id", deleteNoteById)
 
 export default noteRouter
