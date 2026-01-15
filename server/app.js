bad67c994ec2521670b01f304aca03a88eb23429
rajkishor <kothiyalrajkishor1@gmail.com>
2026-01-15 17:20:10 +0530

    user errorMiddleware

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import noteRouter from "./routes/noteRoutes.js";
+import errorMiddleware from "./middleware/errorMiddleware.js";
 
 const app = express();
 
@@ -9,6 +10,6 @@ app.get("/", (req,res)=>{
     res.send("hello i am server")
 })
 app.use("/api/notes", noteRouter)
-
+app.use(errorMiddleware);
 
 export default app
