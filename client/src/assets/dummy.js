b4042784084ab970968e3fb3f475c65c229058d7
rajkishor <kothiyalrajkishor1@gmail.com>
2026-01-19 20:32:40 +0530

    dummy data

---
+++ client/src/assets/dummy.js
@@ -0,0 +1,52 @@
+const notesDummy = [
+  {
+    _id: "1",
+    title: "Learn MERN Backend",
+    content: "Completed CRUD APIs with Express and MongoDB",
+    createdAt: "2026-01-10T10:00:00Z",
+  },
+  {
+    _id: "2",
+    title: "Centralized Error Handling",
+    content: "Implemented global error middleware",
+    createdAt: "2026-01-11T12:30:00Z",
+  },
+  {
+    _id: "3",
+    title: "Frontend Integration",
+    content: "Using Axios service layer for API calls",
+    createdAt: "2026-01-12T09:15:00Z",
+  },
+  {
+    _id: "3",
+    title: "Frontend Integration",
+    content: "Using Axios service layer for API calls",
+    createdAt: "2026-01-12T09:15:00Z",
+  },
+  {
+    _id: "3",
+    title: "Frontend Integration",
+    content: "Using Axios service layer for API calls",
+    createdAt: "2026-01-12T09:15:00Z",
+  },
+  {
+    _id: "3",
+    title: "Frontend Integration",
+    content: "Using Axios service layer for API calls",
+    createdAt: "2026-01-12T09:15:00Z",
+  },
+  {
+    _id: "3",
+    title: "Frontend Integration",
+    content: "Using Axios service layer for API calls",
+    createdAt: "2026-01-12T09:15:00Z",
+  },
+  {
+    _id: "3",
+    title: "Frontend Integration",
+    content: "Using Axios service layer for API calls",
+    createdAt: "2026-01-12T09:15:00Z",
+  },
+];
+
+export default notesDummy;
