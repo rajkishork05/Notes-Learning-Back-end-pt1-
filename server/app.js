5747d60b896f6ba467e1a947c2d3db6a49e3e239
rajkishor <kothiyalrajkishor1@gmail.com>
2026-01-19 20:34:56 +0530

    cors fixed

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import noteRouter from "./routes/noteRoutes.js";
 import errorMiddleware from "./middleware/errorMiddleware.js";
-
+import  cors  from "cors";
 const app = express();
 
 app.use(express.json())
-
+app.use(cors());
 app.get("/", (req,res)=>{
     res.send("hello i am server")
 })
