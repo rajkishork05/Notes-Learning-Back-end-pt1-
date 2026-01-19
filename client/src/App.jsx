c01721a0959b14e72e456fd07e83f7002b8c6640
rajkishor <kothiyalrajkishor1@gmail.com>
2026-01-19 20:31:28 +0530

    router for home & note Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,26 @@
 
-import Card from './component/card'
-import NotesForm from './component/NotesForm'
+import { ToastContainer } from "react-toastify";
+import { Routes, Route } from "react-router-dom";
+import NotePage from './Pages/NotePage';
+import HomePage from './Pages/HomePage';
+
+
+
 
 const App = () => {
+
   return (
-    <div className="min-h-screen w-full flex flex-col items-center p-4">
-
-      {/* Notes Input Section */}
-      <div className="w-full max-w-md mb-6">
-        <NotesForm />
-      </div>
-
-      {/* Notes Display Section */}
-      <div className="w-full max-w-4xl">
-        <Card />
-      </div>
-
-    </div>
+  <div className="
+  min-h-screen w-full
+  bg-gradient-to-br from-blue-300 to-indigo-700
+">
+  <ToastContainer autoClose= {2000} />
+  
+  <Routes>
+    <Route path='/' element={<HomePage/>} />
+    <Route path='/showNote/:id' element={<NotePage/>} />
+  </Routes>
+</div>
   )
 }
 
